14745db679535cb64493a3899afb3345e89ee523
jose edinaldo <edinaldofox@gmail.com>
2019-08-20 23:53:31 -0300

    Update docker-entrypoint.sh
    instalando nova lib

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,9 +6,15 @@ then
 	rm config/env/db.connection.js
 fi
 
+if [ -f "node_modules" ]
+then
+	rm -Rf node_modules
+fi
+
 node create-connection.js
 
 npm install
 npm install -g sequelize-cli
+npm i dotenv --save
 
 node server.js
